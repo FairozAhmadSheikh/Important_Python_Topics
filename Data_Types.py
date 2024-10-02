a6bf6309341da20954a8f6feaf88edab35716740
FairozSheikh <fairoz.marvelminds@gmail.com>
2024-10-02 15:47:56 +0530

    ✅Data_Types

---
+++ Data_Types.py
@@ -0,0 +1,13 @@
+
+# Data Types
+
+number:int=10
+decimal:float=6.5
+text:str="Hello, World !"
+active:bool=False
+
+name:list=["Bob","Marley","King","Kim"]
+coordianates:tuple=(1.5,6.5)
+unique:set={1,2,3,4,5,7}
+data:dict={"name":"King","age":63}
+
---
+++ Type_Annotations.py
@@ -0,0 +1,7 @@
+"""Type Annotations are used  to give errors in IDE only so that a programmer can know what kind of error he is making , But the code will execute even with Errors....
+Consider Type Annotations as Traffic Lights : You can consider to obey traffic light and in some case you might not, and sometimes not following traffic light might cause a disaster
+"""
+
+name:str="Feroz Ahmad"
+age: int="Thirty Six Thousand"
+print(name,age)
